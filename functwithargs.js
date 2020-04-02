7fadc938f0a4d259a897cc284f6854b3789c8f27
foorkan <furqanshabbir2010@gmail.com>
2020-04-02 10:56:17 -0500

    functions with multiple args simple examples

---
+++ functwithargs.js
@@ -0,0 +1,20 @@
+function square(num){
+    console.log(num * num);
+}
+
+//example 1 
+function sum(x,y){
+    console.log(x + y);
+
+}
+sum(14, 12);
+
+
+
+//example 2 
+
+function divide(a, b){
+    console.log(a / b);
+}
+
+divide(4, 2);
