297fab12e3f0c66ada5d0b25bdbb1e31af933a65
foorkan <furqanshabbir2010@gmail.com>
2020-04-02 22:52:07 -0500

    functions within objects

--- higherorderfunctions.js
+++ higherorderfunctions.js
@@ -25,3 +25,16 @@ console.log(operations[1]); // we will get subtract function
 //now if we want to use subtract function that is within the operations array we will do:
 
 console.log(operations[1](100, 4));
+
+//the reason why we would store functions within a array is to use them with loops
+
+for (let num of operations){
+    let result = num(30, 5); //now since num will go thru this array, becoming a different function with each loop, it will then use the number's 30 and 5 in each function
+    console.log(result);
+}
+
+//storing them in a object
+
+const thing = {
+    doSomething: multiple //doSomething is a object and it's value is the multiple function
+}
