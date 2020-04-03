4042f2d27de344a441bea09ac66fc3137c6b2f0e
foorkan <furqanshabbir2010@gmail.com>
2020-04-02 22:23:10 -0500

    storing functions within variables

---
+++ storingfunctions.js
@@ -0,0 +1,9 @@
+//we can store functions within variables
+
+//example 1 
+
+const square = function (num) {
+    return num * num;
+}
+
+square(7); //we just called the function that is within the square variable
