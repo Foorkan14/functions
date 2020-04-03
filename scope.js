e3bcd216ed4a2961273bd02c489cc713b310fdf5
foorkan <furqanshabbir2010@gmail.com>
2020-04-02 21:39:13 -0500

    difference b/w var and let

--- scope.js
+++ scope.js
@@ -2,3 +2,14 @@ function helpMe(){
     let msg = "im on fire";
 }
 msg    //msg here outside the function has no value because it is outside the scope of the helpMe function
+
+
+//example 2 
+
+let bird = "eagle";
+
+function birdVariable(){
+    let bird = "hawk";
+}
+
+//outside the function the bird variable's value is eagle but within the function it is a hawk
